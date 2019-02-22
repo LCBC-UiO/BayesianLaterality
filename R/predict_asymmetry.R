a98fd362191051fccf1ea549a0e237b497f5781b
Øystein Sørensen <oyss@medisin.uio.no>
2019-02-22 16:08:55 +0100

    Updated

--- R/predict_asymmetry.R
+++ R/predict_asymmetry.R
@@ -9,7 +9,6 @@
 #' In additional, an optional column name \code{ID} can be provided, giving the subject ID. If a
 #' subject has multiple measurements, the posterior based on all measurements is provided.
 #'
-#' @param theta Probability of right hemispheric dominance.
 #'
 #' @param mu Vector of mean dichotic listening scores.
 #'
@@ -28,17 +27,16 @@
 #'
 #' predict_asymmetry(data)
 #'
-#' data <- data.frame(ID = c(1, 2, 2),
-#'                    handedness = c("A", "D", "D"),
+#'
+#' data <- data.frame(ID = c(1, 1, 2),
+#'                    handedness = c("A", "A", "D"),
 #'                    listening = c(-10, 20, 10),
 #'                    stringsAsFactors = FALSE)
 #'
-#' predict_asymmetry(data)
-#'
-predict_asymmetry <- function(data, theta = 0.05,
+predict_asymmetry <- function(data,
                               mu = c(10.43, -24.41, 11.52, -24.41),
                               sigma = c(22.8, 28.0, 17.0, 28.0),
-                              rho = c(0.56, 0.27)){
+                              rho = c(0.31, 0.115)){
 
   # Check if data contains an ID column
   if(!"ID" %in% colnames(data)) data$ID = seq(1, nrow(data), by = 1)
@@ -52,32 +50,21 @@ predict_asymmetry <- function(data, theta = 0.05,
   data$handedness <- as.integer(factor(data$handedness, levels = c("A", "D"))) - 1L
 
 
-
   # Split data according to ID
   posterior <- lapply(split(data, data$ID), function(subdata){
-    # Prior, p(\lambda)
-    p_lambda <- c(1 - theta, theta)
+    stopifnot(length(unique(subdata$handedness)) == 1)
+    # p(lambda | x1)
+    ind <- ifelse(subdata$handedness[[1]] == 0, 1, 2)
+    p_lambda_x1 <- c(1 - rho[[ind]], rho[[ind]])
 
-    # Loop over the data for each subject
-    for(i in seq(from = 1, to = nrow(subdata), by = 1)){
-      inds <- probtree[probtree$handedness == subdata$handedness[[i]], ]$index
+    # p(x1 | x2, lambda)
+    ind <- if(subdata$handedness[[1]] == 0) c(1, 2) else c(3, 4)
+    p_x1_cond <- c(
+      do.call(prod, list(truncnorm::ptruncnorm(subdata$listening, a = -100.01, b = 100.01, mean = mu[ind[[1]]], sd = sigma[ind[[1]]]))),
+      do.call(prod, list(truncnorm::ptruncnorm(subdata$listening, a = -100.01, b = 100.01, mean = mu[ind[[2]]], sd = sigma[ind[[2]]]))))
 
-      # p(x1| x2, \lambda)
-      p_x1_cond <- mapply(function(m, s) stats::pnorm(subdata$listening[[i]], mean = m, sd = s), mu[inds], sigma[inds])
-
-      # p(x2 | \lambda)
-      p_x2_cond <- (subdata$handedness[[i]] == 0) * (1 - rho) + (subdata$handedness[[i]] == 1) * rho
-
-      # Multiply to get unnormalized posterior
-      posterior <- p_lambda * p_x1_cond * p_x2_cond
-
-      # Normalize the posterior
-      posterior <- posterior / sum(posterior)
-
-      # Update the prior
-      p_lambda <- posterior
-    }
-    return(posterior)
+    posterior <- p_lambda_x1 * p_x1_cond
+    posterior / sum(posterior)
   })
 
   posterior <- do.call(rbind, posterior)
