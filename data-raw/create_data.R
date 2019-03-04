1b7cfd7a9ea44a43e973e701c9adaa97f9cbc105
Øystein Sørensen <oyss@medisin.uio.no>
2019-03-04 07:25:29 +0100

    Removed unused internal data

--- data-raw/create_data.R
+++
@@ -1,8 +0,0 @@
-# Grid representing possible values
-probtree <- data.frame(
-  handedness = c(0, 0, 1, 1),
-  dominance = c(0, 1, 0, 1),
-  index = 1:4
-)
-
-usethis::use_data(probtree, internal = TRUE)
